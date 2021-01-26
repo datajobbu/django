b04b83e38f256fe7ef643624d2bb66b8befd9ddf
hojuly <hojune.choi.me@gmail.com>
2021-01-26 10:37:55 +0900

    fn 3-11.2 without __init__.py

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from papago import views
+from papago.views import base_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('papago/', include('papago.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index'),
+    path('', base_views.index, name='index'),
 ]
