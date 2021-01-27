4d6798408b1ddbebc14c9c0b86f1b6f42aaa2000
hojuly <hojune.choi.me@gmail.com>
2021-01-27 18:13:45 +0900

    minor

--- papago/views/question_views.py
+++ papago/views/question_views.py
@@ -1,11 +1,10 @@
+from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
 
 from ..forms import QuestionForm
 from ..models import Question
-
 
 @login_required(login_url='common:login')
 def question_create(request):
