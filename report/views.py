68970bc38678c02fb57d8cc514e4136594c5a00e
hojuly <hojune.choi.me@gmail.com>
2021-02-01 12:16:15 +0900

    change report detail html to pdf

--- report/views.py
+++ report/views.py
@@ -7,6 +7,12 @@ from pathlib import Path
 
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+import xhtml2pdf.pisa as pisa
 
 from config import settings
 from .forms import CSVForm
@@ -28,11 +34,25 @@ def index(request):
     return render(request, 'report/report.html', {'aml_list':aml_list})
 
 
-def detail(request, tid):
+'''def detail(request, tid):
     aml_list = CSVUpload.objects.filter(tid=tid).values()
     context = {'aml_list' : aml_list}
-    #aml_list = CSVUpload.objects.filter(tid=tid)
-    #aml_list.delete()
+    return render(request, 'report/report_detail.html', context)'''
 
-    return render(request, 'report/report_detail.html', context)
+ 
+def detail(request, tid):
+    aml_list = CSVUpload.objects.filter(tid=tid).values()
+    context = {'aml_list': aml_list}
+
+    template = get_template('report/pdf.html')
+    html = template.render(context)
+    response = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), response)
+
+    if not pdf.err:
+        return HttpResponse(response.getvalue(), content_type='application/pdf')
+
+    else:
+        return HttpResponse("Error Rendering PDF", status=400)
     
+    return render(request, 'report/pdf.html', context)
